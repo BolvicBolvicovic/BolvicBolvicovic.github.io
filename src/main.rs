0e27216e7fb2dd9f911ec2c86527a1ea81c5ffc2
Bolvic <victor.bolheme@gmail.com>
2024-04-28 00:49:40 +0200

    + | New tests with static components list

--- src/main.rs
+++ src/main.rs
@@ -7,38 +7,66 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-
-    let double_count = move || count() * 2;
+    let counters = (0..5).map(|i| create_signal(i)).collect();
     view! {
-        <button
-            on:click=move |_| {
-                set_count.update(|n| if *n < 255 {*n += 5});
+            <StaticButtons counters=counters/>
+    }
+}
+
+#[component]
+fn StaticButtons(
+    counters: Vec<(ReadSignal<i32>, WriteSignal<i32>)>,
+) -> impl IntoView {
+    counters
+        .into_iter()
+        .map(|(count, set_count)| {
+            view! {
+                <li
+                    style:margin="0"
+                    style:margin-bottom="150px"
+                >
+                     <button
+                         on:click=move |_| {
+                             set_count.update(|n| if *n < 255 {*n += 5});
+                         }
+                         style="position: absolute"
+                         style:background=move || format!("rgb({}, {}, {})", count() % 100, count() % 42, count() % 200)
+                         style:max-width="1000px"
+                         style:min-width="500px"
+                         style:max-height="400px"
+                         style:min-height="150px"
+                         style:font-size="xx-large"
+                         style:text-align="center"
+                         style:border-radius="50px"
+                         style=("--columns", count)
+                         class:red=move || count() % 2 == 1
+                         class:white=move || count() % 2 == 0
+                     >
+                         "Click me: "
+                         {count}
+                     <br/>
+                     <ProgressBar progress=count/>
+                     </button>
+                </li>
             }
-            style="position: absolute"
-            style:left=move || format!("rgb({}, {}, 100)", count(), 100)
-            style:background-color=move || format!("rgb({}, {}, 100)", count(), 100)
-            style:max-width="1000px"
-            style:min-width="500px"
-            style:max-height="500px"
-            style:min-height="250px"
-            style:font-size="xx-large"
-            style:text-align="center"
-            style:border-radius="50px"
-            style=("--columns", count)
-            class:red=move || count() % 2 == 1
-        >
-            "Click me: "
-            {count}
-        <br/>
+    })
+    .collect_view()
+}
+
+#[component]
+fn ProgressBar<F>(
+    #[prop(default = 255)]
+    max: u16,
+    progress: F,
+) -> impl IntoView
+where
+    F: Fn() -> i32 + 'static,
+{
+    view! {
         <progress
-            max="255"
+            max=max
             style:min-width="400px"
-            value=double_count
+            value=progress
         />
-        <br/>
-            "Double Count: "
-            {double_count}
-        </button>
     }
 }
